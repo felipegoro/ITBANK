36a6e08f70fa95eff3e63145621cb360f17207ab
felipegoro2002 <113913661+felipegoro2002@users.noreply.github.com>
2024-08-22 12:27:51 -0300

    convertidor de monedas

--- home.js
+++ home.js
@@ -1,3 +1,23 @@
+async function valorDolar(moneda) {
+    let url = "https://api.bluelytics.com.ar/v2/latest";
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Accede al valor correspondiente según la moneda solicitada
+        if (data[moneda]) {
+            let valor = data[moneda].value_avg;
+            console.log(valor);
+            return valor;
+        } else {
+            console.error('Moneda no encontrada');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+    }
+}
+
 document.getElementById('interestForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -18,3 +38,41 @@ document.getElementById('interestForm').addEventListener('submit', function(even
         <p>Monto total a pagar: <strong>$${montoConInteres.toFixed(2)}</strong></p>
     `;
 });
+
+
+document.getElementById('currencyConverter').addEventListener('submit', async function(event) {
+event.preventDefault();
+
+let montoAConvertir = parseFloat(document.getElementById('montopesos').value);
+let monedaInput = document.getElementById('moneda').value;
+
+let moneda;
+    switch (monedaInput) {
+        case 'Dolar Oficial':
+            moneda = 'oficial';
+            break;
+        case 'Dolar Blue':
+            moneda = 'blue';
+            break;
+        case 'Euro Oficial':
+            moneda = 'oficial_euro';
+            break;
+        case 'Euro Blue':
+            moneda = 'blue_euro';
+            break;
+        default:
+            console.error('Moneda no válida');
+            return;
+    }
+
+let Dolar = await valorDolar(moneda);
+let valorConvertido = montoAConvertir / Dolar;
+
+document.getElementById('resultadoPrestamo').innerHTML = `
+        <p>Son ${valorConvertido.toFixed(2)} ${monedaInput}</p>
+    `;
+});
+
+
+
+

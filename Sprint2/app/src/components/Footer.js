fab060589fe1512bacd56dd871c9eac5f4a0345f
sthernandz <sthernandz@gmail.com>
2024-09-05 18:23:19 -0300

    Images

--- Sprint2/app/src/components/Footer.js
+++ Sprint2/app/src/components/Footer.js
@@ -5,14 +5,14 @@ function Footer() {
     return (
         <footer className='all'>
             <div className="left">
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                    <img src="instagram.png" alt="Botón: Enlace a Instagram"/>
+                <a href="https://www.instagram.com" target="_blank" >
+                    <img src="assets/instagram.png" alt="Botón: Enlace a Instagram"/>
                 </a>
-                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                    <img src="x.png" alt="Botón: Enlace a X (Twitter)"/>
+                <a href="https://www.twitter.com" target="_blank" >
+                    <img src="assets/x.png" alt="Botón: Enlace a X (Twitter)"/>
                 </a>
-                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-                    <img className="yt" src="youtube.png" alt="Botón: Enlace a YouTube"/>
+                <a href="https://www.youtube.com" target="_blank" >
+                    <img className="yt" src="assets/youtube.png" alt="Botón: Enlace a YouTube"/>
                 </a>
             </div>
 
@@ -24,11 +24,11 @@ function Footer() {
             </div>
 
             <div className="right">
-                <a href="https://www.googleplay.com" target="_blank" rel="noopener noreferrer">
-                    <img className="google_play" src="google_play.png" alt="Botón: Descargar en Google Play"/>
+                <a href="https://www.googleplay.com" target="_blank" >
+                    <img className="google_play" src="assets/google_play.png" alt="Botón: Descargar en Google Play"/>
                 </a>
-                <a href="https://www.appstore.com" target="_blank" rel="noopener noreferrer">
-                    <img className="apple_store" src="apple_store.png" alt="Botón: Descargar en App Store"/>
+                <a href="https://www.appstore.com" target="_blank" >
+                    <img className="apple_store" src="assets/apple_store.png" alt="Botón: Descargar en App Store"/>
                 </a>
             </div>
         </footer>
